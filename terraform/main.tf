be1c511a8d99d1c281f38118396a49efab1adc42
gatsby <gatsbyrogerthat@gmail.com>
2025-08-21 00:43:09 +0530

    added all the file

---
+++ terraform/main.tf
@@ -0,0 +1,55 @@
+# Generate random suffix for unique naming
+resource "random_integer" "suffix" {
+  min = 1000
+  max = 9999
+}
+
+# Resource Group
+resource "azurerm_resource_group" "main" {
+  name     = "rg-terraform-poc-${random_integer.suffix.result}"
+  location = var.location
+
+  tags = {
+    environment = var.environment
+    project     = "terraform-poc"
+    created-by  = "terraform"
+  }
+}
+
+# Storage Account for website
+resource "azurerm_storage_account" "website" {
+  name                     = "satfpoc${random_integer.suffix.result}"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  
+  static_website {
+    index_document = "index.html"
+  }
+
+  tags = {
+    environment = var.environment
+    project     = "terraform-poc"
+    created-by  = "terraform"
+  }
+}
+
+# Upload website files
+resource "azurerm_storage_blob" "index" {
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.website.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  source                 = "${path.module}/../web/index.html"
+  content_type          = "text/html"
+}
+
+resource "azurerm_storage_blob" "style" {
+  name                   = "style.css"
+  storage_account_name   = azurerm_storage_account.website.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  source                 = "${path.module}/../web/style.css"
+  content_type          = "text/css"
+}
---
+++ terraform/outputs.tf
@@ -0,0 +1,14 @@
+output "website_url" {
+  description = "URL of your website"
+  value       = azurerm_storage_account.website.primary_web_endpoint
+}
+
+output "resource_group_name" {
+  description = "Name of the resource group"
+  value       = azurerm_resource_group.main.name
+}
+
+output "storage_account_name" {
+  description = "Name of the storage account"
+  value       = azurerm_storage_account.website.name
+}
